a9ebdef8ae7074cd2459862b233105af128cb5a4
Daniel Galbraith <daniel.galbraith@saymosaic.com>
2019-12-11 14:01:35 -0800

    add argparse to LangGen.py, update run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,10 @@
 echo "Running LangGen..."
 
 MDY=$(date +"%m-%d-%y")
-OUT_DIR=output/$MDY
+OUT_DIR=output/$MDY/
 
 mkdir -p $OUT_DIR
 
-python3 LangGen.py
-
-mv lang.txt $OUT_DIR/
-mv lang_dict.bin $OUT_DIR/
+python3 LangGen.py -o $OUT_DIR
 
 echo "Done!"
