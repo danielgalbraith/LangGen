b1942cdfcd7bb01557f01459a78f72396a778a5c
Daniel Galbraith <daniel.galbraith@saymosaic.com>
2019-12-17 16:50:55 -0800

    add more fixes to logic incl. SOV word order flow

--- LangGen.py
+++ LangGen.py
@@ -59,7 +59,7 @@ def special_choice(data, chnum, scd):
 	What to do for Q108 (dealing with Antipassive)
 	What to do for Q109 (dealing with Applicative)
 	What to do for Q143 (dealing with crazy negation tables)
-	What to do for Q144 (dealing with crazy negation word order tables): NEED TO FIX!
+	What to do for Q144 (dealing with crazy negation word order tables)
 	'''
 	start_a = scd[chnum][0]
 	end_a = scd[chnum][1]
@@ -124,7 +124,7 @@ def clean_up_tables(data, chnum, chnum_to_end_dict):
 	What to do for Q10 (get rid of Nasal Vowels in West Africa)
 	What to do for Q39 (get rid of table for Pama-Nyungan)
 	What to do for Q58 (get rid of Number of Possessive Nouns table)
-	What to do for Q90 (get rid of various subtypes of relative clauses tables): NEED TO FIX!
+	What to do for Q90 (get rid of various subtypes of relative clauses tables)
 	What to do for Q130 (get rid of cultural categories table)
 	What to do for Q136 (get rid of M in First Person Singular table)
 	What to do for Q137 (get rid of M in Second Person Singular table)
@@ -200,10 +200,60 @@ def remove_contradictions(choice_dict, data_dict):
 	if choice_dict['69'] == "No tense-aspect inflection":
 		choice_dict['67'] == "No inflectional marking of future/non-future distinction"
 	# Imperatives:
-	if choice_dict['70'] in ["The language has morphologically dedicated second plural imperatives but no morphologically dedicated second singular imperatives",
-								"The language has no morphologically dedicated second-person imperatives at all"]:
+	if choice_dict['70'] == "The language has morphologically dedicated second plural imperatives but no morphologically dedicated second singular imperatives":
 		choice_dict['71'] = random_choice(data_dict['71'], 6, 10)[0]
-		# Fix "maximal vs minimal" imperative-hortative issue (chapter 72)
+		choice_dict['72'] = random_choice(['The language has a maximal system, but not a minimal one', '133', 'The language has neither a maximal nor a minimal system', '201'], 0, 4)[0]
+	elif choice_dict['70'] == "The language has no morphologically dedicated second-person imperatives at all":
+		choice_dict['71'] = random_choice(data_dict['71'], 6, 10)[0]
+		choice_dict['72'] = "The language has neither a maximal nor a minimal system"		
+	# Evidentials:
+	if choice_dict['77'] == "No grammatical evidentials":
+		choice_dict['78'] = "No grammatical evidentials"
+	# Word order:
+	if choice_dict['81'] == "Subject-object-verb (SOV)":
+		choice_dict['82'] = "Subject precedes verb (SV)"
+		choice_dict['83'] = "Object precedes verb (OV)"
+		choice_dict['84'] = random_choice(data_dict['84'], 2, 14)[0]
+		choice_dict['95'] = random_choice(['Object-verb and postpositional (OV&Postp)', '472', 'Object-verb and prepositional (OV&Prep)', '14', 'Languages not falling into one of the preceding four types', '158'], 0, 6)[0]
+		choice_dict['96'] = random_choice(['Object-verb and relative clause-noun (OV&RelN)', '132', 'Object-verb and noun-relative clause (OV&NRel)', '113', 'Languages not falling into one of the preceding four types', '213'], 0, 6)[0]
+		if choice_dict['96'] == "Object-verb and relative clause-noun (OV&RelN)":
+			choice_dict['90'] = "Relative clause precedes noun (RelN)"
+		elif choice_dict['96'] == "Object-verb and noun-relative clause (OV&NRel)":
+			choice_dict['90'] = "Relative clause follows noun (NRel)"
+		else:
+			choice_dict['90'] = random_choice(['Internally-headed relative clause', '24', 'Correlative relative clause', '7', 'Adjoined relative clause', '8', 'Double-headed relative clause', '1', 'Mixed types of relative clause with none dominant', '64'], 0, 10)[0]
+		choice_dict['97'] = random_choice(['Object-verb and adjective-noun (OV&AdjN)', '216', 'Object-verb and noun-adjective (OV&NAdj)', '332', 'Languages not falling into one of the preceding four types', '198'], 0, 6)[0]
+		if choice_dict['97'] == "Object-verb and adjective-noun (OV&AdjN)":
+			choice_dict['87'] = "Modifying adjective precedes noun (AdjN)"
+		elif choice_dict['97'] == "Object-verb and noun-adjective (OV&NAdj)":
+			choice_dict['87'] = "Modifying adjective follows noun (NAdj)"
+		else:
+			choice_dict['87'] = random_choice(['Both orders of noun and modifying adjective occur, with neither dominant', '110', 'Adjectives do not modify nouns, occurring as predicates in internally headed relative clauses', '5'], 0, 4)[0]
+		choice_dict['144'] = random_choice(['NegSOV', '11', 'SNegOV', '15', 'SONegV', '65', 'SOVNeg', '49', 'More than one position for negative morpheme, with none dominant', '91',
+			'Optional single negation', '1', 'Obligatory double negation', '101', 'Optional double negation', '67', 'Morphological negation only (but not double negation)', '333',
+			'Other languages', '168'], 0, 20)[0]
+	elif choice_dict['81'] == "Subject-verb-object (SVO)":
+		...
+	elif choice_dict['81'] == "Verb-subject-object (VSO)":
+		...
+	elif choice_dict['81'] == "Verb-object-subject (VOS)":
+		...
+	elif choice_dict['81'] == "Object-verb-subject (OVS)":
+		...
+	elif choice_dict['81'] == "Object-subject-verb (OSV)":
+		...
+	elif choice_dict['81'] == "Lacking a dominant word order":
+		...
+	elif choice_dict['81'] == "SOV or SVO":
+		...
+	elif choice_dict['81'] == "VSO or VOS":
+		...
+	elif choice_dict['81'] == "SVO or VSO":
+		...
+	elif choice_dict['81'] == "SVO or VOS":
+		...
+	else: # SOV or OVS
+
 
 def write_to_file(out_dir, chnames, chnums, choice_dict, run_idx):
 	with open(out_dir + ('lang_%s.txt' % run_idx), "w") as f:
